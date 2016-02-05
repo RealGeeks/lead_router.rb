667f07a3f8287349d4c898130a35d2c5a6b2cb07
Igor Sobreira <igor@igorsobreira.com>
2016-02-04 17:14:35 -1000

    refactor tests to avoid duplication

--- test/test_lead_router.rb
+++ test/test_lead_router.rb
@@ -1,50 +1,64 @@
 require 'minitest/autorun'
 require 'webmock/minitest'
+require 'mocha/mini_test'
 
 require 'lead_router'
 
 
 class LeadRouterTest < Minitest::Test
 
+  #
+  # Make sure all public api methods call 'request' private method correctly
+  #
+
   def test_create_lead
-    stub_request(:any, /.*/)
+    lr = LeadRouter.new("api.com", "LM", "secret")
+    lr.expects(:request).with(:post, 'http://api.com/rest/sites/site-123/leads',
+                              '{"email":"lead@gmail.com"}')
 
-    lr = LeadRouter.new("leadrouter.com", "LeadManager", "secret")
     lr.create_lead("site-123", {email: "lead@gmail.com"})
-
-    assert_request_performed(:post, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/leads",
-                             '{"email":"lead@gmail.com"}')
   end
 
   def test_update_lead
-    stub_request(:any, /.*/)
-
     lr = LeadRouter.new("leadrouter.com", "LeadManager", "secret")
-    lr.update_lead("site-123", "lead-abc", {email: "lead@gmail.com"})
-
-    assert_request_performed(:patch, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/leads/lead-abc",
+    lr.expects(:request).with(:patch,  "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/leads/lead-abc",
                              '{"email":"lead@gmail.com"}')
+
+    lr.update_lead("site-123", "lead-abc", {email: "lead@gmail.com"})
   end
 
   def test_add_activities
-    stub_request(:any, /.*/)
-
     lr = LeadRouter.new("leadrouter.com", "LeadManager", "secret")
-    lr.add_activities("site-123", "lead-abc", [{'type' => 'one'}, {'type' => 'two'}])
+    lr.expects(:request).with(:post, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/leads/lead-abc/activities",
+                              '[{"type":"one"},{"type":"two"}]')
 
-    assert_request_performed(:post, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/leads/lead-abc/activities",
-                             '[{"type":"one"},{"type":"two"}]')
+    lr.add_activities("site-123", "lead-abc", [{'type' => 'one'}, {'type' => 'two'}])
   end
 
   def test_add_potential_seller_lead
+    lr = LeadRouter.new("leadrouter.com", "LeadManager", "secret")
+    lr.expects(:request).with(:post, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/potential-seller-leads",
+                              '{"id":"abc"}')
+
+    lr.create_potential_seller_lead("site-123", {id: "abc"})
+  end
+
+  #
+  # Test 'request' method
+  #
+
+  def test_request
     stub_request(:any, /.*/)
 
-    lr = LeadRouter.new("leadrouter.com", "LeadManager", "secret")
-    lr.create_potential_seller_lead("site-123", {id: "abc"})
+    lr = LeadRouter.new("leadrouter.com", "LM", "secret")
+    lr.send(:request, :post, "http://api.com/leads", '{"id":"123"}')
 
-    assert_request_performed(:post, "http://LeadManager:secret@leadrouter.com/rest/sites/site-123/potential-seller-leads",
-                             '{"id":"abc"}')
+    assert_request_performed(:post, "http://LM:secret@api.com/leads", '{"id":"123"}')
   end
+
+  #
+  # Custom asserts
+  #
 
   private
 
