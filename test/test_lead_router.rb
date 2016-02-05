bda23e021eda047eb2ae1671def1ec8514142eb1
Igor Sobreira <igor@igorsobreira.com>
2016-02-04 19:24:51 -1000

    remove duplication from tests extracting fixture

--- test/test_lead_router.rb
+++ test/test_lead_router.rb
@@ -6,41 +6,40 @@ require 'lead_router'
 
 
 class LeadRouterTest < Minitest::Test
+  extend Minitest::Spec::DSL
+
+  let(:client) { LeadRouter.new("api.com", "LM", "secret") }
 
   #
   # Make sure all public api methods call 'request' private method correctly
   #
 
   def test_create_lead
-    lr = LeadRouter.new("api.com", "LM", "secret")
-    lr.expects(:request).with(:post, 'http://api.com/rest/sites/site-123/leads',
-                              '{"email":"lead@gmail.com"}')
+    client.expects(:request).with(:post, 'http://api.com/rest/sites/site-123/leads',
+                                  '{"email":"lead@gmail.com"}')
 
-    lr.create_lead("site-123", {email: "lead@gmail.com"})
+    client.create_lead("site-123", {email: "lead@gmail.com"})
   end
 
   def test_update_lead
-    lr = LeadRouter.new("api.com", "LM", "secret")
-    lr.expects(:request).with(:patch,  "http://api.com/rest/sites/site-123/leads/lead-abc",
-                             '{"email":"lead@gmail.com"}')
+    client.expects(:request).with(:patch,  "http://api.com/rest/sites/site-123/leads/lead-abc",
+                                  '{"email":"lead@gmail.com"}')
 
-    lr.update_lead("site-123", "lead-abc", {email: "lead@gmail.com"})
+    client.update_lead("site-123", "lead-abc", {email: "lead@gmail.com"})
   end
 
   def test_add_activities
-    lr = LeadRouter.new("api.com", "LM", "secret")
-    lr.expects(:request).with(:post, "http://api.com/rest/sites/site-123/leads/lead-abc/activities",
-                              '[{"type":"one"},{"type":"two"}]')
+    client.expects(:request).with(:post, "http://api.com/rest/sites/site-123/leads/lead-abc/activities",
+                                  '[{"type":"one"},{"type":"two"}]')
 
-    lr.add_activities("site-123", "lead-abc", [{'type' => 'one'}, {'type' => 'two'}])
+    client.add_activities("site-123", "lead-abc", [{'type' => 'one'}, {'type' => 'two'}])
   end
 
   def test_add_potential_seller_lead
-    lr = LeadRouter.new("api.com", "LM", "secret")
-    lr.expects(:request).with(:post, "http://api.com/rest/sites/site-123/potential-seller-leads",
-                              '{"id":"abc"}')
+    client.expects(:request).with(:post, "http://api.com/rest/sites/site-123/potential-seller-leads",
+                                  '{"id":"abc"}')
 
-    lr.create_potential_seller_lead("site-123", {id: "abc"})
+    client.create_potential_seller_lead("site-123", {id: "abc"})
   end
 
   #
